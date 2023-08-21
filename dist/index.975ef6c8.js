e20cf4cbc9c21a65f21b2d08e84dcb5e9047fc95
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-08-21 17:27:49 -0400

    navbar styling

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -1236,9 +1236,11 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "default", ()=>toggleTheme);
 function toggleTheme() {
     let currentTheme = "dark";
+    const toggle = document.getElementById("toggle-knob");
     document.getElementById("toggle-color-scheme").addEventListener("click", function() {
         document.body.classList.toggle("dark-theme");
         document.body.classList.toggle("light-theme");
+        toggle.classList.toggle("toggle-knob-forward");
         currentTheme === "dark" ? currentTheme = "light" : currentTheme = "dark";
     });
 }
--- src/scripts/toggle-theme.js
+++ src/scripts/toggle-theme.js
@@ -1,9 +1,11 @@
 export default function toggleTheme() {
     let currentTheme = "dark";
+    const toggle = document.getElementById('toggle-knob');
 
     document.getElementById('toggle-color-scheme').addEventListener('click', function() {
         document.body.classList.toggle('dark-theme');
         document.body.classList.toggle('light-theme');
+        toggle.classList.toggle('toggle-knob-forward');
         currentTheme === "dark" ? currentTheme = "light" : currentTheme = "dark";
     });
 }
