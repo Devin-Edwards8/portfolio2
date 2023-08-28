fe21c7fb42160459c3ac3ecfe647713c9c7f661b
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2023-08-28 12:04:18 -0400

    added email copying and started mobile formatting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import typewrite from "./scripts/typewriter";
 import toggleTheme from "./scripts/toggle-theme";
+import copyEmail from "./scripts/copy-email";
 
 typewrite();
 toggleTheme();
+copyEmail();
